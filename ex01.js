48111558478cca72599c8c83b6cf0b72ec2c9f0a
Pantakit-Joseph <mr.pantakit@gmail.com>
2023-07-11 12:00:31 +0700

    update ex01

--- ex01.js
+++ ex01.js
@@ -8,4 +8,6 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  console.log("welcome");
+  console.log(`URL: http://localhost:${port}`);
 });
