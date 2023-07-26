f79e07d334272df4a21c5ca96e1e58f4a1adfbe7
Pantakit-Joseph <mr.pantakit@gmail.com>
2023-07-26 10:26:47 +0700

    .

--- ex02.js
+++ ex02.js
@@ -25,15 +25,116 @@ const db = knex({
 app.use(cors());
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
+  console.log("test api");
   res.send({ ok: 1 });
 });
 
+// app.get("/list", async (req, res) => {
+//   const data = await db("users_student").where("major_id", 98);
+//   res.send({
+//     data: data,
+//     status: 1,
+//   });
+// });
+
 app.get("/list", async (req, res) => {
-  const data = await db("users_student").where("major_id", 98);
+  const data = await db("member");
   res.send({
     data: data,
-    status: 1,
+    ok: 1,
   });
+});
+
+app.get("/member", async (req, res) => {
+  const id = req.query.id;
+  const data = await db("member").where("id", id).first();
+  res.send({
+    data: data,
+    ok: 1,
+  });
+});
+
+app.get("/insert", async (req, res) => {
+  if (!req.query.name || !req.query.password || !req.query.dep) {
+    res.send({
+      ok: 0,
+      error: "validate failed",
+    });
+  }
+  let ids;
+  try {
+    ids = await db("member").insert({
+      username: req.query.name,
+      password: req.query.password,
+      dep: req.query.dep,
+    });
+    res.send({
+      ok: 1,
+      ids: ids[0],
+    });
+  } catch (error) {
+    res.send({
+      ok: 0,
+      error: error.message,
+    });
+  }
+});
+
+app.get("/update", async (req, res) => {
+  if (
+    !req.query.id ||
+    !req.query.username ||
+    !req.query.password ||
+    !req.query.dep
+  ) {
+    res.send({
+      ok: 0,
+      error: "validate failed",
+    });
+  }
+  let result;
+  try {
+    result = await db("member").where("id", req.query.id).update({
+      username: req.query.username,
+      password: req.query.password,
+      dep: req.query.dep,
+    });
+    res.send({
+      ok: 1,
+      result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      ok: 0,
+      error: error.message,
+      code: error?.code,
+    });
+  }
+});
+
+app.get("/delete", async (req, res) => {
+  if (!req.query.id) {
+    res.send({
+      ok: 0,
+      error: "validate failed",
+    });
+  }
+  let ids;
+  try {
+    ids = await db("member").where("id", req.query.id).del();
+    res.send({
+      ok: 1,
+      ids: ids[0],
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({
+      ok: 0,
+      error: error.message,
+      code: error?.code,
+    });
+  }
 });
 
 app.listen(7001, () => {
